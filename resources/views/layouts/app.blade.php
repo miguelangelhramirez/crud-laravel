e0f556b7d76a362af0dcc7d145ffd48f2c1ab00b
miguelangelhramirez <100546932+miguelangelhramirez@users.noreply.github.com>
2024-10-31 22:56:05 -0500

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD</title>
-    <link rel="stylesheet" href="{{ asset('styles.css') }}">
+    <link rel="stylesheet" href="styles.css">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>    
